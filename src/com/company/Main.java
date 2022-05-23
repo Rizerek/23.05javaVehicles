459ef1003adb89a415a4dc98c38e961db182921e
Uczen <@>
2022-05-23 14:59:33 +0200

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.company.classes.Engines.CombustionEngine;
+import com.company.classes.Engines.ElectricEngine;
 import com.company.classes.Vehicles.*;
 
 public class Main {
@@ -10,6 +12,31 @@ public class Main {
         Tram tram1;
         DeliveryVan deliveryVan1;
         FloatPlane floatPlane1;
-        JetFighter jetFighter;
+        JetFighter jetFighter1;
+        Bulldozer bulldozer1;
+        Motorbike motorbike1;
+        Bicycle bicycle1;
+        Helicopter helicopter1;
+        CarAmfibia carAmfibia1;
+        CombustionEngine combustionEngine1;
+        ElectricEngine electricEngine1;
+        BicycleDrive bicycleDrive1;
+        Sedan sedan2;
+        Bus bus2;
+        Tram tram2;
+        DeliveryVan deliveryVan2;
+        FloatPlane floatPlane2;
+        JetFighter jetFighter2;
+        Bulldozer bulldozer2;
+        Motorbike motorbike2;
+        Bicycle bicycle2;
+        Helicopter helicopter2;
+        CarAmfibia carAmfibia2;
+        CombustionEngine combustionEngine2;
+        ElectricEngine electricEngine2;
+        BicycleDrive bicycleDrive2;
+        var[] tab1 = {sedan1,bus1};
+
+
     }
 }
